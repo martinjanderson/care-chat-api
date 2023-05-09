79ebef48b26037220e0c4b88e3120cfded014155
Martin Anderson <46332+martinjanderson@users.noreply.github.com>
2023-05-09 12:45:05 -0700

    Adding firebase auth middleware and helper scripts for generating tokens

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,27 @@
 import express from 'express';
+import authMiddleware from './authMiddleware';
 
 const app = express();
 const port = 3000;
+const router = express.Router();
 
-app.get('/healthcheck', (req, res) => {
+router.use(authMiddleware);
+
+// Configuration for a Firebase Firestore connection
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+
+const db = getFirestore();
+
+router.get('/healthcheck', (req, res) => {
   res.sendStatus(200);
 });
 
+app.use(router);
+
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running on port: ${port}`);
 });
+
+
+export default app;
