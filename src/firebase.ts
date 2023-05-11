7f0f0ca9290c7c5b620108d23c0d6190003fb927
Martin Anderson <46332+martinjanderson@users.noreply.github.com>
2023-05-11 12:23:40 -0700

    firebase should init from ADC in production

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,8 +1,5 @@
 import * as admin from 'firebase-admin';
-import * as serviceAccount from '../keys/care-chat-386021-1a8b55a04e7f.json';
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-});
+admin.initializeApp();
 
 export default admin;
