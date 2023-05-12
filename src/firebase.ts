fe868a2256a9be72317f1bc2e6f086c8957d607f
Martin Anderson <46332+martinjanderson@users.noreply.github.com>
2023-05-12 09:04:14 -0700

    Load GOOGLE_APPLICATION_CREDENTIALS from env in development

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,4 +1,11 @@
 import * as admin from 'firebase-admin';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+if (process.env.GOOGLE_APPLICATION_CREDENTIALS_PATH) {
+  process.env.GOOGLE_APPLICATION_CREDENTIALS = process.env.GOOGLE_APPLICATION_CREDENTIALS_PATH;
+}
 
 admin.initializeApp();
 
